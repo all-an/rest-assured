e3ebea917b8faa72f6b36cefbb41c3e5c2d0de04
all-an <all-an@hotmail.com>
2022-03-10 17:00:48 -0300

    first simple test

--- src/main/java/main/restassured/HiRestAssured.java
+++ src/main/java/main/restassured/HiRestAssured.java
@@ -8,7 +8,11 @@ public class HiRestAssured {
 	
 	public static void main(String[] args) {
 		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
-		System.out.println(response.getBody().asString());
+		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+		System.out.println(response.statusCode() == 200);
+		
+		System.out.println(response.getBody().asString().equals("Olá Teste Erro!"));
+		System.out.println(response.statusCode() == 201);
 	}
 
 }
