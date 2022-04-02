414d7d6490c8261417720a5904bb8362fc5da142
all-an <all-an@hotmail.com>
2022-04-02 17:14:47 -0300

    second level

--- src/main/java/main/restassured/UserJsonTest.java
+++ src/main/java/main/restassured/UserJsonTest.java
@@ -3,8 +3,14 @@ package main.restassured;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 
 public class UserJsonTest {
 
@@ -22,6 +28,30 @@ public class UserJsonTest {
 			.body("name", containsString("Silva"))
 			.body("age", greaterThan(18))
 		;
+		
+	}
+	
+	@Test
+	public void mustVerifyFirstLevelOtherWays() {
+		Response response = RestAssured.request(Method.GET, url + "1");
+		
+		//path
+		System.out.println(response.path("id"));
+		
+		assertEquals(Integer.valueOf(1), response.path("id"));
+		assertEquals(Integer.valueOf(1), response.path("%s" , "id"));
+	
+		//jsonpath
+		JsonPath jpath = new JsonPath(response.asString());
+		assertEquals(1, jpath.getInt("id"));
+		
+		//from 
+		int id = JsonPath.from(response.asString()).getInt("id");
+		assertEquals(1, id);
+	}
+	
+	@Test
+	public void mustVerifySecondLevel() {
 		
 	}
 	
