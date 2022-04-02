9b3cc9f23b12f0517f245d47584f36fbaede32af
all-an <all-an@hotmail.com>
2022-04-02 17:30:57 -0300

    json

--- src/main/java/main/restassured/UserJsonTest.java
+++ src/main/java/main/restassured/UserJsonTest.java
@@ -52,7 +52,30 @@ public class UserJsonTest {
 	
 	@Test
 	public void mustVerifySecondLevel() {
-		
+		given()
+		.when()
+			.get(url + "2")
+		.then()
+			.statusCode(200)
+			.body("name", containsString("Joaquina"))
+			.body("endereco.rua", is("Rua dos bobos"))
+		;
+	}
+	
+	@Test
+	public void mustAssertList() {
+		given()
+		.when()
+			.get(url + "3")
+		.then()
+			.statusCode(200)
+			.body("name", containsString("Ana"))
+			.body("filhos", hasSize(2))
+			.body("filhos[0].name", is("Zezinho"))
+			.body("filhos[1].name", is("Luizinho"))
+			.body("filhos.name", hasItem("Zezinho"))
+			.body("filhos.name", hasItems("Zezinho", "Luizinho"))
+		;
 	}
 	
 }
