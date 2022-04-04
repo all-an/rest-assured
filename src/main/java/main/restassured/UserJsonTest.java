5060f36e032fb7b6800e5d81f97bc2469b849077
all-an <all-an@hotmail.com>
2022-04-04 07:51:36 -0300

    advanced assertions using groovy like code

--- src/main/java/main/restassured/UserJsonTest.java
+++ src/main/java/main/restassured/UserJsonTest.java
@@ -121,8 +121,25 @@ public class UserJsonTest {
 			.body("$", hasSize(3)) // $ root list
 			.body("", hasSize(3)) // root
 			.body("age.findAll{it <= 25}.size()", is(2))
+			.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+			.body("find{it.age <= 25}.name", is("Maria Joaquina"))
+			.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina","Ana Júlia"))
+			.body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+			.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", 
+					allOf(
+							arrayContaining("MARIA JOAQUINA"), 
+							arrayWithSize(1)) 
+					)
+			.body("age.collect{it * 2}", hasItems(60, 50, 40))
+			.body("id.max()", is(3))
+			.body("salary.min()", is(1234.5678f))
+			//.body("salary.findAll{it != null}.sum()", is(3734.5678f)) // error decimal places
+			.body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001)))
+			.body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)))
 		;
-		
 	}
 		
 		
