c54a2851c5db2c1b0d0e0470f43727a769c2ad96
all-an <all-an@hotmail.com>
2022-04-11 20:05:29 -0300

    auth test

---
+++ src/main/java/main/restassured/AuthTest.java
@@ -0,0 +1,175 @@
+package main.restassured;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.XmlPath.CompatibilityMode;
+
+public class AuthTest {
+
+	@Test
+	public void deveAcessarSWAPI() {
+		given()
+			.log().all()
+		.when()
+			.get("https://swapi.dev/api/people/1")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("Luke Skywalker"))
+		;
+	}
+	
+	
+	//  http://api.openweathermap.org/data/2.5/weather?q=Fortaleza,BR&appid=29178e691ea4287d83568b08778215c8&units=metric
+	//957194fa4a92fee5eb71bb40bfc038d5
+	@Test
+	public void deveObterClima() {
+		given()
+			.log().all()
+			.queryParam("q", "Fortaleza,BR")
+			.queryParam("appid", "c9d2f59749e99181fa0c3ebf72541d48")
+			.queryParam("units", "metric")
+		.when()
+			.get("http://api.openweathermap.org/data/2.5/weather")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("Fortaleza"))
+			.body("coord.lon", is(-38.52f))
+			.body("main.temp", greaterThan(25f))
+		;
+	}
+	
+	@Test
+	public void naoDeveAcessarSemSenha() {
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(401)
+		;
+	}
+
+	@Test
+	public void deveFazerAutenticacaoBasica() {
+		given()
+			.log().all()
+		.when()
+			.get("https://admin:senha@restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
+		;
+	}
+
+	@Test
+	public void deveFazerAutenticacaoBasica2() {
+		given()
+			.log().all()
+			.auth().basic("admin", "senha")
+		.when()
+			.get("https://restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
+		;
+	}
+	
+	@Test
+	public void deveFazerAutenticacaoBasicaChallenge() {
+		given()
+			.log().all()
+			.auth().preemptive().basic("admin", "senha")
+		.when()
+			.get("https://restapi.wcaquino.me/basicauth2")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
+		;
+	}
+	
+	@Test
+	public void deveFazerAutenticacaoComTokenJWT() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "allan@allan.com");
+		login.put("senha", "123456");
+		
+		//Login na api
+		//Receber o token
+		String token = given()
+			.log().all()
+			.body(login)
+			.contentType(ContentType.JSON)
+		.when()
+			.post("http://barrigarest.wcaquino.me/signin")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.extract().path("token");
+		;
+		System.out.println(token);
+		
+		//Obter as contas
+		given()
+			.log().all()
+			.header("Authorization", "JWT " + token)
+		.when()
+			.get("http://barrigarest.wcaquino.me/contas")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("nome", hasItem("Conta Allan"))
+		;
+	}
+	
+	@Test
+	public void deveAcessarAplicacaoWeb() {
+		//login
+		String cookie = given()
+			.log().all()
+			.formParam("email", "allan@allan.com")
+			.formParam("senha", "123456")
+			.contentType(ContentType.URLENC.withCharset("UTF-8"))
+		.when()
+			.post("http://seubarriga.wcaquino.me/logar")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.extract().header("set-cookie");
+		;
+		
+		cookie = cookie.split("=")[1].split(";")[0];
+		System.out.println(cookie);
+		
+		//obter conta
+		
+		String body = given()
+			.log().all()
+			.cookie("connect.sid", cookie)
+		.when()
+			.get("http://seubarriga.wcaquino.me/contas")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("html.body.table.tbody.tr[0].td[0]", is("Conta Allan"))
+			.extract().body().asString();
+		;
+		
+		System.out.println("--------------");
+		XmlPath xmlPath = new XmlPath(CompatibilityMode.HTML, body);
+		System.out.println(xmlPath.getString("html.body.table.tbody.tr[0].td[0]"));
+	}
+
+}
